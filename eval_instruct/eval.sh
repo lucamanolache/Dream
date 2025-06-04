108fd153bd8058ba3bcbd7678cb7578a49540ac1
zhxieml <fffffarmer@sjtu.edu.cn>
2025-06-04 21:40:55 +0800

    add missing HF_ALLOW_CODE_EVAL

--- eval_instruct/eval.sh
+++ eval_instruct/eval.sh
@@ -75,7 +75,7 @@ HF_ALLOW_CODE_EVAL=1 PYTHONPATH=. accelerate launch --main_process_port 12334 -m
     --log_samples --confirm_run_unsafe_code \
     --apply_chat_template
 
-PYTHONPATH=. accelerate launch --main_process_port 12334 -m lm_eval \
+HF_ALLOW_CODE_EVAL=1 PYTHONPATH=. accelerate launch --main_process_port 12334 -m lm_eval \
     --model diffllm \
     --model_args pretrained=Dream-org/Dream-v0-Instruct-7B,trust_remote_code=True,max_new_tokens=1280,diffusion_steps=1280,dtype="bfloat16",temperature=0.1,top_p=0.9,alg="entropy" \
     --tasks humaneval_instruct \
